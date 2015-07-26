6071afa3af1f325db6b5a65a97280f7a7519bba5
pollirrata <jorgegaona@yahoo.com>
2015-07-26 17:07:15 -0500

    Fixing errors

--- cachematrix.R
+++ cachematrix.R
@@ -8,11 +8,13 @@
 ### Create the functions that will be part of the list that will be returned.
 ### This functions will allow to set the values for the main data items: the
 ### original matrix and its inverse. We use setters and getters for each one.
+
 makeCacheMatrix <- function(x = matrix()) {
     myInverse <- NULL
     set <- function(matrix){
         x <<- matrix
         myInverse <<- NULL
+        message("Cleaning cache")
     }
     get <- function() {
        x
@@ -32,9 +34,11 @@ makeCacheMatrix <- function(x = matrix()) {
 
 
 ## Write a short comment describing this function
-
+### First we get the value of the inversed matrix. If 'NULL', it means
+### either the original matrix has been changed or we setted a new one, 
+### so we need to calculate the inverse again. If we have a value already,
+### then it means we can use it as cached and avoid the computation.
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
     inversed <- x$getInverse()
     if(is.null(inversed)){
         matrix <- x$get()
